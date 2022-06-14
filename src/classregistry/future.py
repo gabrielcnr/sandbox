2a077293c57686872796ef4c7a59a81d33ba4066
Gabriel Reis <gabrielcnr@gmail.com>
2022-06-14 23:17:48 +0100

    example of failure

--- src/classregistry/future.py
+++ src/classregistry/future.py
@@ -6,3 +6,11 @@ class Future(Trade):
 
     def func(self) -> str:
         return 'Future here'
+
+
+# Uncomment the lines below to see the error in import time
+# class Future2(Trade):
+#     trade_type = 'FUTURE'  # already registered
+#
+#     def func(self):
+#         return 'Back to the Future'
