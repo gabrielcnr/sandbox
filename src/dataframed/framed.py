600ceaba82a2c420ddb3aa1e8240edd58041d5f3
Gabriel Reis <gabrielcnr@gmail.com>
2022-06-16 09:33:07 +0100

    implemented setitem

--- src/dataframed/framed.py
+++ src/dataframed/framed.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import pandas as pd
 
 # TODO: how about the order of the columns? What if one wants/needs to enforce order?
@@ -8,6 +9,9 @@ from pandas._typing import Axes, Dtype
 
 
 class DataFramed(pd.DataFrame):
+    """
+    A pandas DataFrame Proxy object that allows schema definition and validation.
+    """
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         Schema = getattr(cls, 'Schema', None)
@@ -18,7 +22,6 @@ class DataFramed(pd.DataFrame):
             type_hints = {}
 
         cls._type_hints = type_hints
-
 
     def __init__(
         self,
@@ -50,8 +53,20 @@ class DataFramed(pd.DataFrame):
                                         f' to be {type_} - got: {type(value).__name__}')
                 else:
                     # it is iterable
-                    if isinstance(value, str):
-                        # TODO: TBC ... (I stopped here) ...
+                    if isinstance(value, str) and type_ is not str:  # TODO: support bytes and other user strings?
+                        raise TypeError(f'{type(self).__name__} column: {key} needs type to be'
+                                        f' to be {type_} - got: {type(value).__name__}')
+                    else:
+                        # if it's given a generator we're going to extinguish it here... # TODO: generator case
+                        for v in value:
+                            if not isinstance(v, type_):
+                                raise TypeError(f'{type(self).__name__} column: {key} needs type to be'
+                                                f' to be {type_} - got: {type(v).__name__}')
+
+
+        print('calling super', repr(key), repr(value))
+        # If it didn't raise, then it's good to proceed ...
+        super().__setitem__(key, value)
 
     def __delitem__(self, key):
         if key in self._type_hints:
@@ -60,7 +75,17 @@ class DataFramed(pd.DataFrame):
         else:
             return super().__delitem__(key)
 
+    def copy(self):
+        return type(self)(self)
 
+
+    # TODO: the DataFrame API is quite long, so this Proxy object will need to implement a lot more 
+    #       to be really comprehensive
+
+
+
+
+# Tests
 
 def test_simple_creation():
 
@@ -82,7 +107,7 @@ def test_assert_isinstance():
     assert isinstance(MyDataFrame(), pd.DataFrame)
 
 
-def test_cannot_delete_or_drop_columns_that_belong_to_schema():
+def test_cannot_delete_columns_that_belong_to_schema():
     class MyDataFrame(DataFramed):
         class Schema:
             name: str
@@ -92,6 +117,49 @@ def test_cannot_delete_or_drop_columns_that_belong_to_schema():
     with pytest.raises(RuntimeError):
         del df['name']
 
+
+def test_cannot_drop_columns_that_belong_to_schema():
+    class MyDataFrame(DataFramed):
+        class Schema:
+            name: str
+
+    df = MyDataFrame(data=[{'name': 'joe'}, {'name': 'jane'}])
+
     # TODO: this fails... it doesn't call __delitem__ under the hood...
     with pytest.raises(RuntimeError):
         df.drop(columns=['name'])
+
+
+def test_type_checking_on_column_assignment():
+    class MyDataFrame(DataFramed):
+        class Schema:
+            name: str
+
+    df = MyDataFrame()
+    df['name'] = ['Alice', 'Bob']
+
+    assert list(df['name']) == ['Alice', 'Bob']
+
+    df['name'] = 'Charlie'
+
+    assert list(df['name']) == ['Charlie', 'Charlie']
+
+    with pytest.raises(TypeError):
+        df['name'] = 1
+
+    with pytest.raises(TypeError):
+        df['name'] = ['Alice', object()]
+
+    # after all this we check that it's still the last successful one
+    assert list(df['name']) == ['Charlie', 'Charlie']
+
+
+def test_copy_returns_same_type():
+    class MyDataFrame(DataFramed):
+        class Schema:
+            name: str
+
+    df = MyDataFrame()
+    df_copy = df.copy()
+
+    assert type(df_copy) is MyDataFrame
